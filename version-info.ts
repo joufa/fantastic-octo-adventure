3cc3fa4b4ad0a8b614406d75755bcd077115dc9c
Antti Reijonen <areijone@gmail.com>
2020-01-23 19:48:25 +0200

    Fix git-version directory

--- version-info.ts
+++ version-info.ts
@@ -1,7 +1,7 @@
 export const versionInfo = (() => {
   try {
     // tslint:disable-next-line:no-var-requires
-    return require('../../git-version.json');
+    return require('git-version.json');
   } catch {
     return { tag: 'v0.0.0', hash: 'dev' };
   }
